f28ab4ef17ea3f73b54f1100a0771917396b1c4f
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-10-11 23:09:08 +0300

    feat: transform app component into class, add form component, change global styles

--- lesson-1/src/App.tsx
+++ lesson-1/src/App.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import { Component } from 'react';
+import { Form } from './components';
+import styles from './App.module.scss';
 
-function App() {
-  return <div className='App'></div>;
+interface AppProps {}
+interface AppStates {
+  title: string;
+}
+
+class App extends Component<AppProps, AppStates> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      title: 'Title',
+    };
+  }
+
+  handleTitleChange = (text: string) => {
+    this.setState({
+      title: text,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <main className={styles.main}>
+          <section className={styles.section}>
+            <h1 className={styles.title}>{this.state.title}</h1>
+            <Form
+              btnText='Change title'
+              placeholder='Enter text'
+              inputName='text'
+              formName='title-change'
+              onSubmit={this.handleTitleChange}
+            />
+          </section>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
